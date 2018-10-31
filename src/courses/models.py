768ab5f121cf8a6dd706c669c834eb1f3b41af7e
Mihai Alexandru Manolescu <alex.inntekt@gmail.com>
2018-10-31 20:08:31 +0200

    -reached 1:00:02

--- src/courses/models.py
+++ src/courses/models.py
@@ -19,7 +19,9 @@ class Course(models.Model):
 	def get_absolute_url(self):
 		return reverse('courses:detail', kwargs={'slug':self.slug})
 
-
+	@property
+	def lessons(self):
+		return self.lesson_set.all().order_by('position')
 
 class Lesson(models.Model):
 	slug = models.SlugField()
@@ -33,6 +35,11 @@ class Lesson(models.Model):
 		return self.title
 
 
+	def get_absolute_url(self):
+		return reverse('courses:lesson-detail', kwargs={
+			'course_slug': self.course.slug,
+			'lesson_slug': self.slug
+			})
 
 
 
--- src/courses/views.py
+++ src/courses/views.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 
 from .models import Course, Lesson
 
@@ -14,3 +14,89 @@ class CourseListView(ListView):
 
 class CourseDetailView(DetailView):
 	model = Course
+
+class LessonDetailView(View):
+	def get(self, request, course_slug, lesson_slug, *args, **kwargs):
+		course_qs = Course.objects.filter(slug=course_slug)
+		if course_qs.exists():
+			course = course_qs.first()
+
+		lesson_qs = course.lessons.filter(slug=lesson_slug)
+		if lesson_qs.exists():
+			lesson = lesson_qs.first()
+
+
+		context = {
+			'object': lesson
+		}
+
+		return render(request, "courses/lesson_detail.html", context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
