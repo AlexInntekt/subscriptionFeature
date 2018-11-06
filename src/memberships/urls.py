dd4de2338db8f5d978e8e96199dddf691ee8e457
Mihai Alexandru Manolescu <alex.inntekt@gmail.com>
2018-11-06 05:02:48 +0100

    testing a token UI

--- src/memberships/urls.py
+++ src/memberships/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-
+from django.views.generic import TemplateView
 from . views import MembershipSelectView, PaymentView
 app_name = 'memberships'
 
 urlpatterns = [
 	path('', MembershipSelectView.as_view(), name='select'),
 	path('payment', PaymentView, name='payment'),
+	path('test', TemplateView.as_view(template_name='memberships/test.html'), name='test'),
+
 	
 ]
